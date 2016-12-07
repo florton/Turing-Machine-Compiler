8af9ce88c32bef13d2c09092e0904985bb05737a
Flanders Lorton <florton@lion.lmu.edu>
2016-12-06 23:56:01 -0800

    Added Machine Files + Example

---
+++ Scanner.py
@@ -0,0 +1,53 @@
+import sys
+import re
+
+def Scan(file):
+    try:
+        turingMachine = open(file, 'r')
+    except:
+        raise RuntimeError("Please specify a valid turing language file")
+        
+    stateRegex = re.compile("(\w+)={(start|accept|reject|),?(\S*)}")
+    edgeRegex = re.compile(",?\((\S)->(\S),(R|S|L),(\w+)\)")
+    inputRegex = re.compile("Input\((\S*)\)")
+    outputRegex = re.compile("Output\(((\d+|),?(\d+|))\)")
+
+    input = ""
+    states = {}
+    output = None
+    for line in turingMachine:
+        line = line.replace(" ", "")
+        reMatch = re.match(stateRegex,line)
+        if reMatch is not None:
+            parsedEdges = {}
+            stateName = reMatch.group(1)
+            modifier = reMatch.group(2)
+            line = reMatch.group(3)
+            edges = re.findall(edgeRegex,line)
+            for edge in edges:
+                readChar = edge[0]
+                writeChar = edge[1]
+                nextMove = edge[2]
+                nextState = edge[3]
+                parsedEdges[readChar] = (writeChar,nextMove,nextState)
+            states[stateName] = {'modifier':modifier,'edges':parsedEdges}
+            continue
+        reMatch = re.match(inputRegex,line)
+        if reMatch is not None:
+            input = reMatch.group(1)
+            continue
+        reMatch = re.match(outputRegex,line)
+        if reMatch is not None:
+            output = [reMatch.group(2),reMatch.group(3)]
+            continue
+        raise RuntimeError("Error Parsing Line: " + line)
+    return (input,output,states)
+        
+if __name__ == "__main__":
+    output = Scan(sys.argv[1])
+    print output[0]
+    print output[1]
+    for state in output[2]:
+        print state
+        for edge in output[2][state]:
+            print str(edge) +':' + str(output[2][state][edge])
---
+++ TuringMachine.py
@@ -0,0 +1,72 @@
+import sys
+from Scanner import Scan
+
+try:
+    machine = Scan(sys.argv[1])
+except:
+    raise RuntimeError("Please specify a valid turing language file")
+    
+input = machine[0]
+output = machine[1]
+states = machine[2]
+tape = []
+    
+def Run():
+
+    #Process Input
+    for char in input:
+        tape.append(char)
+    tape.append('#')
+
+    #Load start, accept & reject states
+    start = None
+    accept = []
+    reject = []
+    for name, state in states.iteritems():
+        if state['modifier'] == 'start':
+            start = state
+        elif state['modifier'] == 'accept':
+            accept.append(state)
+        elif state['modifier'] == 'reject':
+            reject.append(state)    
+
+    #Run Through States
+    tapePosition = 0
+    currentState = start
+    while(True):
+        if tape[-1] != '#':
+            tape.append('#')
+            
+        if currentState in accept:
+            Output()
+            return True
+        if currentState in reject:
+            Output()
+            return False
+            
+        #Run deterministic edge
+        currentEdge = currentState['edges'][str(tape[tapePosition])]
+        tape[tapePosition] = currentEdge[0]
+        if currentEdge[1] == 'R':
+            tapePosition = tapePosition +1
+        elif currentEdge[1] == 'L':
+            tapePosition = tapePosition-1 if tapePosition!=0 else 0
+        currentState = states[currentEdge[2]]
+
+def Output():
+    #Print Output (Tape Subsection)
+    if output is not None:
+        final = ''
+        start = 0
+        end = len(tape)-1
+        if output[0] != '':
+            start = int(output[0])
+        if output[1] != '':    
+            end = int(output[1])
+            
+        for char in tape[start:end]:
+                final = final + char
+        print final
+
+if __name__ == "__main__":
+    print Run()
